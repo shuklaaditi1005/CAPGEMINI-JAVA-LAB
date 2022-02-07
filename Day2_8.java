188254d30369ad03d8e9b78116a13c58f786b4d4
shuklaaditi1005 <54732686+shuklaaditi1005@users.noreply.github.com>
2022-02-07 10:02:55 +0530

    Add files via upload

---
+++ Day2_8.java
@@ -0,0 +1,12 @@
+package com.xyz;
+
+public class Day2_8 {
+	public static void main(String[] args) {
+	    int i = 1, n = 5;
+	    while(i <= n) {
+	      System.out.println(i);
+	      i++;
+	    }
+	  }
+
+}
